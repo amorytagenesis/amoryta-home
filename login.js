ad4d6b90c87cf0694113a9c4823cecc4a55b14b7
amorytagenesis <amoryta.genesis@gmail.com>
2026-07-05 08:50:33 -0400

    login.js

--- login.js
+++ login.js
@@ -1,1 +1,21 @@
+document.getElementById("loginForm").addEventListener("submit", function(e){
 
+e.preventDefault();
+
+const member = JSON.parse(localStorage.getItem("amorytaMember"));
+
+const phone = document.getElementById("phone").value;
+
+const passkey = document.getElementById("passkey").value;
+
+if(member && member.phone === phone && member.passkey === passkey){
+
+window.location.href = "welcome.html";
+
+}else{
+
+alert("🌱 We couldn't find your journey. Please check your PASSKEY or begin a new journey.");
+
+}
+
+});
