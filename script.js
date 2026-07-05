c13ecee5902e53a9af28145becbebf8450d8bd43
amorytagenesis <amoryta.genesis@gmail.com>
2026-07-05 05:38:38 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Welcome Home, Amity.");
+document.getElementById("joinForm").addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    const member = {
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        passkey: document.getElementById("passkey").value,
+        team: document.getElementById("team").value,
+        bucket: document.getElementById("bucket").value,
+        memberID: "AM-" + Date.now()
+    };
+
+    localStorage.setItem("amorytaMember", JSON.stringify(member));
+
+    alert("🌱 Welcome to AMORYTA!");
+
+    window.location.href = "welcome.html";
+
+});
