9b457d16b038e57893488d1f3e2f9e716c42469d
amorytagenesis <amoryta.genesis@gmail.com>
2026-07-08 09:02:51 -0700

    config.js

--- config.js
+++ config.js
@@ -1,1 +1,21 @@
+/*
+========================================
+AMORYTA Genesis Alpha
+Configuration File
+========================================
+*/
 
+const CONFIG = {
+
+    APP_NAME: "AMORYTA",
+
+    VERSION: "Genesis Alpha v0.3.0",
+
+    /*
+    Replace this after we deploy
+    the Google Apps Script Web App.
+    */
+
+    API_URL: ""
+
+};
