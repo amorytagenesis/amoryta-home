e642c9657efe5d93d303631bd828ab67aaa20d2e
amorytagenesis <amoryta.genesis@gmail.com>
2026-07-12 10:53:47 -0700

    api.js

--- api.js
+++ api.js
@@ -1,1 +1,33 @@
+/*
+=========================================================
+AMORYTA Genesis
+ROOM 006
+API BRIDGE
+Version : 1.0.0
+Status  : Active
+=========================================================
+*/
 
+/*
+=========================================================
+POST REQUEST
+=========================================================
+*/
+
+async function postRequest(data) {
+
+  const response = await fetch(CONFIG.API_URL, {
+
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json"
+    },
+
+    body: JSON.stringify(data)
+
+  });
+
+  return await response.json();
+
+}
